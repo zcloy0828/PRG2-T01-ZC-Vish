5c93f99d43bf12d4597ef4269da841c45953af6a
zcloy0828 <s10267362@connect.np.edu.sg>
2025-01-23 20:05:31 +0800

    added cfftflight class

---
+++ PRG2-T01-ZC-Vish/CFFTFlight.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG2_T01_ZC_Vish
+{
+    internal class CFFTFlight
+    {
+        private double requestFee;
+        public double RequestFee
+        {
+            get { return requestFee; }
+            set { requestFee = value; }
+        }
+
+        public CFFTFlight() { }
+
+        public CFFTFlight(string flightNumber, string origin, string destination, DateTime expectedTime, string status, double requestFee) : base(flightNumber, origin, destination, expectedTime, status)
+        {
+            RequestFee = requestFee;
+        }
+
+        public override double CalculateFees()
+
+        public override string ToString()
+        {
+            return base.ToString() + "RequestFee: " + RequestFee;
+        }
+    }
+}
